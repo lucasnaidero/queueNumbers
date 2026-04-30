d52e6c418ff11383c3e0534d7b0e865dd3564168
lucasnaidero <39132309+lucasnaidero@users.noreply.github.com>
2026-04-30 16:45:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use(express.json());
+//app.use(express.json());
 
 // Coda in memoria
 let queue = [];
